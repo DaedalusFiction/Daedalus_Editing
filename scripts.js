183a2964e725868ccf0432a651639cb0a5fcd7d0
David Sorensen <teardownthismall@gmail.com>
2021-08-20 18:13:15 -0400

    opacity intersection stuff

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 const name = document.getElementById('name').focus();
+const numSteps = 20.0;
 
-window.addEventListener("load", (slide) => {
-    servicesElement = document.querySelector('#servicesItem');
-    firstElement = document.querySelector("#firstOption");
+let servicesElement;
+let prevRatio = 0.0;
+
+window.addEventListener("load", (event) => {
+    servicesElement = document.querySelector('#servicesSection');
+
     createObserver();
 } )
 
@@ -13,15 +17,36 @@ function createObserver() {
     let options = {
         root: document.querySelector(null),
         rootMargin: "0px",
-        threshold: 0.2
+        threshold: [0.2, 0.3]
+        //threshold: buildThresholdList()
     };
 
     observer = new IntersectionObserver(handleIntersect, options);
     observer.observe(servicesElement);
 }
 
-function handleIntersect(entries, observer) {
-    //firstElement.classList.add("sr1");
-    alert("hi");
+// function buildThresholdList() {
+//     let thresholds = [];
+//     let numSteps = 20;
 
+//     for (let i=1.0; 1<=numSteps; i++) {
+//         let ratio = i/numSteps;
+//         thresholds.push(ratio);
+//     }
+
+//     thresholds.push(0);
+//     return thresholds;
+// }
+
+function handleIntersect(entries, observer) {
+    console.log(entries);
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > prevRatio) {
+            console.log(entry.intersectionRatio);
+            entry.target.style.opacity = entry.intersectionRatio;
+        } 
+
+        prevRatio = entry.intersectionRatio;
+    });
+    
 }
