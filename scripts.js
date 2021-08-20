ed80b89265b7a2b7101f317f2d36800bdcd381f2
David Sorensen <teardownthismall@gmail.com>
2021-08-20 14:26:18 -0400

    intersection handler

--- scripts.js
+++ scripts.js
@@ -1,2 +1,27 @@
 const name = document.getElementById('name').focus();
-  
+
+window.addEventListener("load", (slide) => {
+    servicesElement = document.querySelector('#servicesItem');
+    firstElement = document.querySelector("#firstOption");
+    createObserver();
+} )
+
+function createObserver() {
+    let observer;
+    
+
+    let options = {
+        root: document.querySelector(null),
+        rootMargin: "0px",
+        threshold: 0.2
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(servicesElement);
+}
+
+function handleIntersect(entries, observer) {
+    //firstElement.classList.add("sr1");
+    alert("hi");
+
+}
