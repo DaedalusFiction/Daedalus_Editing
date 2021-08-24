b7a25bc0e992335f8935c66166d2670d6a0c746d
David Sorensen <teardownthismall@gmail.com>
2021-08-24 14:36:16 -0400

    photo etc.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const name = document.getElementById('name').focus();
+//const name = document.getElementById('name').focus();
 const sr1List = document.querySelectorAll('.sr1-wrapper');
 const sr2List = document.querySelectorAll('.sr2-wrapper');
 
@@ -35,53 +35,3 @@ sr2List.forEach(el => {
 
 
 
-// const numSteps = 20.0;
-
-// let servicesElement;
-// let prevRatio = 0.0;
-
-// window.addEventListener("load", (event) => {
-//     servicesElement = document.querySelector('#servicesSection');
-
-//     createObserver();
-// } )
-
-// function createObserver() {
-//     let observer;
-    
-
-//     let options = {
-//         root: document.querySelector(null),
-//         rootMargin: "0px",
-//         threshold: [0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-//         //threshold: buildThresholdList()
-//     };
-
-//     observer = new IntersectionObserver(handleIntersect, options);
-//     observer.observe(servicesElement);
-// }
-
-// function buildThresholdList() {
-//     let thresholds = [];
-//     let numSteps = 20;
-
-//     for (let i=1.0; 1<=numSteps; i++) {
-//         let ratio = i/numSteps;
-//         thresholds.push(ratio);
-//     }
-
-//     thresholds.push(0);
-//     return thresholds;
-// }
-
-function handleIntersect(entries, observer) {
-    console.log(entries);
-    entries.forEach((entry) => {
-        if (entry.intersectionRatio > prevRatio) {
-            console.log(entry.intersectionRatio);
-        } 
-
-        prevRatio = entry.intersectionRatio;
-    });
-    
-}
