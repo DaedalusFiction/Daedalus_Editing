a117a4d9c3112547bbb8e3bb17db0102085f077c
David Sorensen <teardownthismall@gmail.com>
2021-09-07 13:42:06 -0400

    cleaned up js file

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 const serviceCards = document.querySelectorAll('.service-option');
 const headers = document.querySelectorAll("h2");
 
-// onload fade-in animation
-//const heroText = document.getElementById('heroText').style.opacity = '1'; 
-
-
 
 // Observers
 
@@ -44,17 +40,6 @@ const popUpObserver = new IntersectionObserver(entries => {
   )
 });
 
-
-
-function AddAnimationDurations(elementsArray, increaseAmount) {
-  // Adds incrementing animation durations so that elements will appear to cascade 
-  let counter = 0.0;
-  elementsArray.forEach(el => {
-    el.style.animationDuration = counter.toString() + 's';
-    counter += increaseAmount;
-  });
-}
-
 AddAnimationDurations(serviceCards, 0.2);
 
 serviceCards.forEach(el => {
@@ -65,56 +50,13 @@ headers.forEach(el => {
   popUpObserver.observe(el);
 });
 
+// Helper Functions
 
-
-//const name = document.getElementById('name').focus();
-// const sr1List = document.querySelectorAll('.sr1-wrapper');
-// const sr2List = document.querySelectorAll('.sr2-wrapper');
-
-
-// Services Slider
-
-// const observer = new IntersectionObserver(entries => {
-
-  //     entries.forEach(entry => { 
-    
-//         if (entry.isIntersecting && entry.target.classList.contains('sr1-wrapper')) {
-  //             entry.target.classList.add('slideFromLeft');
-  //             return;
-  //         } else if (entry.isIntersecting && entry.target.classList.contains('sr2-wrapper')) {
-    //             entry.target.classList.add('slideFromRight');
-    //             return;
-    //         } 
-    
-    
-    //         entry.target.classList.remove('slideFromLeft');
-    //         entry.target.classList.remove('slideFromRight');
-    
-    //     })
-    
-    // });
-    
-    // sr1List.forEach(el => {
-      //     observer.observe(el);
-      // });
-      // sr2List.forEach(el => {
-        //     observer.observe(el);
-        // });
-        
-        // Navbar Transform
-        
-        // window.onscroll = function() {scrollFunction()};
-        
-        // function scrollFunction() {
-        //   if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
-        //     document.getElementById("logo").setAttribute("src", "images/daedalusIconSmall.png");
-        //   } else {
-        //     document.getElementById("logo").setAttribute("src", "images/block-version-rev-b-darkbg.png");
-        //   }
-        // }
-        
-        
-        
-        
-        
-        
+function AddAnimationDurations(elementsArray, increaseAmount) {
+  // Adds incrementing animation durations so that elements will appear to cascade 
+  let counter = 0.0;
+  elementsArray.forEach(el => {
+    el.style.animationDuration = counter.toString() + 's';
+    counter += increaseAmount;
+  });
+}
