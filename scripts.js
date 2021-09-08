38a28fd6c37379d1075a9b0814f6de6108058d13
David Sorensen <teardownthismall@gmail.com>
2021-09-08 10:38:26 -0400

    added animations on honestly too many things, doesn't look good but it was fun

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const serviceCards = document.querySelectorAll('.service-option');
-const headers = document.querySelectorAll("h2");
+const buttons = document.querySelectorAll(".btn");
+const fadeInFromLeftElements = document.querySelectorAll(".left-fade");
+const fadeInFromRightElements = document.querySelectorAll(".right-fade");
 
 
 // Observers
@@ -40,14 +42,46 @@ const popUpObserver = new IntersectionObserver(entries => {
   )
 });
 
+const fadeInFromLeftObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fadeInFromLeft');
+      return;
+    }
+    entry.target.classList.remove('fadeInFromLeft');
+    entry.target.style.opacity = '0';
+  }
+  )
+});
+
+const fadeInFromRightObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fadeInFromRight');
+      return;
+    }
+    entry.target.classList.remove('fadeInFromRight');
+    entry.target.style.opacity = '0';
+  }
+  )
+});
+
 AddAnimationDurations(serviceCards, 0.2);
 
 serviceCards.forEach(el => {
   fadeInAndUpObserver.observe(el);
 });
 
-headers.forEach(el => {
+buttons.forEach(el => {
   popUpObserver.observe(el);
+});
+
+fadeInFromLeftElements.forEach(el => {
+  fadeInFromLeftObserver.observe(el);
+});
+
+fadeInFromRightElements.forEach(el => {
+  fadeInFromRightObserver.observe(el);
 });
 
 // Helper Functions
